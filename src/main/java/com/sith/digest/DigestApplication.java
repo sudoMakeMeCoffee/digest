3d5d9189d9be7ab56ce3d609f30e9bc8869f7314
sith <skdev.techno@gmail.com>
2025-07-23 23:10:24 +0530

    initial commit

---
+++ src/main/java/com/sith/digest/DigestApplication.java
@@ -0,0 +1,13 @@
+package com.sith.digest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sith/digest/DigestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sith.digest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
